386eed39592b823bea674763c9044c0ac74ce8cb
Reza IF <rezaif79.ri@gmail.com>
2022-03-30 20:58:20 +0700

    Reformat app index code

--- pages/app/index.js
+++ pages/app/index.js
@@ -1,149 +1,170 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function App() {
-  
-  const [firstName, setFirstName] = useState("")
-  const [lastName, setLastName] = useState("")
-  const [email, setEmail] = useState("")
-  const [number, setNumber] = useState("")
-  const [address, setAddress] = useState("")
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [address, setAddress] = useState("");
 
-  
-  return(
+  return (
     <div className="container mx-auto">
-    <div className="hidden sm:block" aria-hidden="true">
-      <div className="py-5">
-        <div className="border-t border-gray-200" />
-      </div>
-    </div>
-
-    <div className="mt-10 sm:mt-0 bg-gray-200">
-      <div className="md:grid md:grid-cols-3 md:gap-6 p-5">
-        <div className="md:col-span-1">
-          <div className="p-10 ">
-            <h3 className="text-xl font-medium leading-6 text-indigo-700">Dashboard - User Data</h3>
-            <p className="mt-1 text-md text-gray-700">
-              This form is used to input new user data, 
-              Please fill all the field required in the form
-            </p>
-          </div>
+      <div className="hidden sm:block" aria-hidden="true">
+        <div className="py-5">
+          <div className="border-t border-gray-200" />
         </div>
-        <div className="mt-5 md:mt-0 md:col-span-2">
-          <form action="#" method="POST">
-            <div className="shadow overflow-hidden sm:rounded-md">
-              <div className="px-4 py-5 bg-white sm:p-6">
-                <div className="grid grid-cols-6 gap-6">
-                  <div className="col-span-6 sm:col-span-3">
-                    <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
-                      First name
-                    </label>
-                    <input
-                      type="text"
-                      name="first-name"
-                      id="first-name"
-                      autoComplete="given-name"
-                      required={true}
-                      maxLength={25}
-                      value={firstName}
-                      onChange={(e) => {setFirstName(e.target.value)}}
-                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                    />
-                  </div>
+      </div>
 
-                  <div className="col-span-6 sm:col-span-3">
-                    <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
-                      Last name
-                    </label>
-                    <input
-                      type="text"
-                      name="last-name"
-                      id="last-name"
-                      autoComplete="family-name"
-                      required={true}
-                      maxLength={25}
-                      value={lastName}
-                      onChange={(e) => {setLastName(e.target.value)}}
-                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                    />
-                  </div>
+      <div className="mt-10 sm:mt-0 bg-gray-200">
+        <div className="md:grid md:grid-cols-3 md:gap-6 p-5">
+          <div className="md:col-span-1">
+            <div className="p-10 ">
+              <h3 className="text-xl font-medium leading-6 text-indigo-700">
+                Dashboard - User Data
+              </h3>
+              <p className="mt-1 text-md text-gray-700">
+                This form is used to input new user data, Please fill all the
+                field required in the form
+              </p>
+            </div>
+          </div>
+          <div className="mt-5 md:mt-0 md:col-span-2">
+            <form action="#" method="POST">
+              <div className="shadow overflow-hidden sm:rounded-md">
+                <div className="px-4 py-5 bg-white sm:p-6">
+                  <div className="grid grid-cols-6 gap-6">
+                    <div className="col-span-6 sm:col-span-3">
+                      <label
+                        htmlFor="first-name"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        First name
+                      </label>
+                      <input
+                        type="text"
+                        name="first-name"
+                        id="first-name"
+                        autoComplete="given-name"
+                        required={true}
+                        maxLength={25}
+                        value={firstName}
+                        onChange={(e) => {
+                          setFirstName(e.target.value);
+                        }}
+                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
 
-                  <div className="col-span-6 sm:col-span-4">
-                    <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
-                      Email address
-                    </label>
-                    <input
-                      type="text"
-                      name="email-address"
-                      id="email-address"
-                      autoComplete="email"
-                      required={true}
-                      maxLength={50}
-                      value={email}
-                      onChange={(e) => {setEmail(e.target.value)}}
-                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                    />
-                  </div>
+                    <div className="col-span-6 sm:col-span-3">
+                      <label
+                        htmlFor="last-name"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Last name
+                      </label>
+                      <input
+                        type="text"
+                        name="last-name"
+                        id="last-name"
+                        autoComplete="family-name"
+                        required={true}
+                        maxLength={25}
+                        value={lastName}
+                        onChange={(e) => {
+                          setLastName(e.target.value);
+                        }}
+                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
 
-                  <div className="col-span-6 sm:col-span-3">
-                    <label htmlFor="country" className="block text-sm font-medium text-gray-700">
-                      Phone number
-                    </label>
-                    <input
-                      type="text"
-                      id="phone-number"
-                      name="phone-number"
-                      value={number}
-                      onChange={(e) => {setNumber(e.target.value.replace(/\D/g, ""))}}
-                      maxLength={12}
-                      minLength={8}
-                      required={true}
-                      className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    >
-                    </input>
-                  </div>
+                    <div className="col-span-6 sm:col-span-4">
+                      <label
+                        htmlFor="email-address"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Email address
+                      </label>
+                      <input
+                        type="text"
+                        name="email-address"
+                        id="email-address"
+                        autoComplete="email"
+                        required={true}
+                        maxLength={50}
+                        value={email}
+                        onChange={(e) => {
+                          setEmail(e.target.value);
+                        }}
+                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
 
-                  <div className="col-span-6">
-                    <label htmlFor="street-address" className="block text-sm font-medium text-gray-700">
-                      Street address
-                    </label>
-                    <input
-                      type="text"
-                      name="street-address"
-                      id="street-address"
-                      autoComplete="street-address"
-                      required={true}
-                      maxLength={100}
-                      value={address}
-                      onChange={(e) => {setAddress(e.target.value)}}
-                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                    />
-                  </div>
+                    <div className="col-span-6 sm:col-span-3">
+                      <label
+                        htmlFor="country"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Phone number
+                      </label>
+                      <input
+                        type="text"
+                        id="phone-number"
+                        name="phone-number"
+                        value={number}
+                        onChange={(e) => {
+                          setNumber(e.target.value.replace(/\D/g, ""));
+                        }}
+                        maxLength={12}
+                        minLength={8}
+                        required={true}
+                        className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      ></input>
+                    </div>
 
+                    <div className="col-span-6">
+                      <label
+                        htmlFor="street-address"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        Street address
+                      </label>
+                      <input
+                        type="text"
+                        name="street-address"
+                        id="street-address"
+                        autoComplete="street-address"
+                        required={true}
+                        maxLength={100}
+                        value={address}
+                        onChange={(e) => {
+                          setAddress(e.target.value);
+                        }}
+                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                  <button
+                    type="submit"
+                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Send
+                  </button>
                 </div>
               </div>
-              <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                <button
-                  type="submit"
-                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                  Send
-                </button>
-              </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div className="hidden sm:block" aria-hidden="true">
-      <div className="py-5">
-        <div className="border-t border-gray-200" />
+      <div className="hidden sm:block" aria-hidden="true">
+        <div className="py-5">
+          <div className="border-t border-gray-200" />
+        </div>
       </div>
-    </div>
 
-    <div>
-      This is the data table
+      <div>This is the data table</div>
     </div>
-  </div>
-  )
+  );
 }
